48cd4e49d659672ee53194861401b49f6ec70de1
Jin Huang <jhuang@bnl.gov>
2021-06-03 14:29:08 -0400

    detach event eval from subsystem eval

--- common/G4_EventEvaluator.C
+++ common/G4_EventEvaluator.C
@@ -17,7 +17,7 @@ namespace EVENT_EVALUATOR
 {
   int Verbosity = 0;
   float EnergyThreshold = 0.05;
-}  // namespace EventEvaluator
+}  // namespace EVENT_EVALUATOR
 
 void Event_Eval(const std::string &filename)
 {
@@ -27,7 +27,7 @@ void Event_Eval(const std::string &filename)
   eval->set_reco_tracing_energy_threshold(EVENT_EVALUATOR::EnergyThreshold);
   eval->Verbosity(EVENT_EVALUATOR::Verbosity);
 
-  if (Enable::TRACKING_EVAL)
+  if (Enable::TRACKING)
   {
     eval->set_do_TRACKS(true);
     eval->set_do_HITS(true);
@@ -35,13 +35,13 @@ void Event_Eval(const std::string &filename)
     if (G4TRACKING::DISPLACED_VERTEX)
       eval->set_do_VERTEX(true);
   }
-  if (Enable::CEMC_EVAL) eval->set_do_CEMC(true);
-  if (Enable::EEMC_EVAL) eval->set_do_EEMC(true);
-  if (Enable::FEMC_EVAL) eval->set_do_FEMC(true);
-  if (Enable::HCALIN_EVAL) eval->set_do_HCALIN(true);
-  if (Enable::HCALOUT_EVAL) eval->set_do_HCALOUT(true);
-  if (Enable::FHCAL_EVAL) eval->set_do_FHCAL(true);
-  if (Enable::FHCAL_EVAL || Enable::FEMC_EVAL || Enable::EEMC_EVAL)
+  if (Enable::CEMC_CLUSTER) eval->set_do_CEMC(true);
+  if (Enable::EEMC_CLUSTER) eval->set_do_EEMC(true);
+  if (Enable::FEMC_CLUSTER) eval->set_do_FEMC(true);
+  if (Enable::HCALIN_CLUSTER) eval->set_do_HCALIN(true);
+  if (Enable::HCALOUT_CLUSTER) eval->set_do_HCALOUT(true);
+  if (Enable::FHCAL_CLUSTER) eval->set_do_FHCAL(true);
+  if (Enable::FHCAL_CLUSTER || Enable::FEMC_CLUSTER || Enable::EEMC_CLUSTER)
     eval->set_do_CLUSTERS(true);
 
   eval->set_do_MCPARTICLES(true);
