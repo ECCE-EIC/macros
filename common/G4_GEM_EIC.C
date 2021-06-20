0c2ef20569709d40077741c8334ecce8980f2289
Jin Huang <jhuang@bnl.gov>
2021-06-20 17:37:54 -0400

    resolve GEM conflict

--- common/G4_GEM_EIC.C
+++ common/G4_GEM_EIC.C
@@ -45,14 +45,14 @@ void BGEM_Init()
 
 void EGEMSetup(PHG4Reco *g4Reco)
 {
-  make_GEM_station("EGEM_0", g4Reco, -160.0, -1.5, -3.5);
-  make_GEM_station("EGEM_1", g4Reco, -190.0, -1.65, -3.6);
+  make_GEM_station("EGEM_0", g4Reco, -160.0, -1.65, -3.5);
+  make_GEM_station("EGEM_1", g4Reco, -190.0, -1.85, -3.6);
 }
 
 void FGEMSetup(PHG4Reco *g4Reco, const int N_Sector = 8)
 {
   ///////////////////////////////////////////////////////////////////////////
-  make_GEM_station("FGEM_0", g4Reco, 160.0, 1.55, 3.5, N_Sector);
+  make_GEM_station("FGEM_0", g4Reco, 160.0, 1.65, 3.5, N_Sector);
 
   ///////////////////////////////////////////////////////////////////////////
   make_GEM_station("FGEM_1", g4Reco, 285.0, 1.2, 3.5, N_Sector);
