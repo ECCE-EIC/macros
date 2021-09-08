74a6c2d13a6e2c18072b254279c3b9604766c858
John Lajoie <lajoie@iastate.edu>
2021-09-08 15:24:27 -0500

    macro changes for BECAL and LFHCAL projections (#45)
    Co-authored-by: cdean-github <59485912+cdean-github@users.noreply.github.com>

--- common/G4_Tracking_EIC.C
+++ common/G4_Tracking_EIC.C
@@ -29,9 +29,11 @@ namespace G4TRACKING
   bool PROJECTION_EEMC = false;
   bool PROJECTION_EHCAL = false;
   bool PROJECTION_CEMC = false;
+  bool PROJECTION_BECAL = false;
   bool PROJECTION_HCALOUT = false;
   bool PROJECTION_FEMC = false;
   bool PROJECTION_FHCAL = false;
+  bool PROJECTION_LFHCAL = false;
 }  // namespace G4TRACKING
 
 //-----------------------------------------------------------------------------//
@@ -97,6 +99,14 @@ void Tracking_Reco()
     TRACKING::ProjectionNames.insert("FHCAL");
   }
   //-------------------------
+  // LFHCAL
+  //-------------------------
+  if (Enable::LFHCAL && G4TRACKING::PROJECTION_LFHCAL)
+  {
+    TRACKING::FastKalmanFilter->add_state_name("LFHCAL");
+    TRACKING::ProjectionNames.insert("LFHCAL");
+  }
+  //-------------------------
   // CEMC
   //-------------------------
   if (Enable::CEMC && G4TRACKING::PROJECTION_CEMC)
@@ -104,6 +114,13 @@ void Tracking_Reco()
     TRACKING::FastKalmanFilter->add_state_name("CEMC");
     TRACKING::ProjectionNames.insert("CEMC");
   }
+  //-------------------------
+  // BECAL
+  //-------------------------
+  if (Enable::BECAL && G4TRACKING::PROJECTION_BECAL)
+  {
+    TRACKING::FastKalmanFilter->add_state_name("BECAL");
+    TRACKING::ProjectionNames.insert("BECAL");
   //-------------------------
   // HCALOUT
   //-------------------------
