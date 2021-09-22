585c330252f44bea79c6bd50e97aa1a688b7ce8a
cdean-github <59485912+cdean-github@users.noreply.github.com>
2021-09-21 22:37:31 -0600

    Reenabled single particle production

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -102,7 +102,7 @@ int Fun4All_G4_EICDetector(
   //   Input::SARTRE = true;
 
   // Simple multi particle generator in eta/phi/pt ranges
-  //Input::SIMPLE = true;
+  Input::SIMPLE = true;
   // Input::SIMPLE_NUMBER = 2; // if you need 2 of them
   // Input::SIMPLE_VERBOSITY = 1;
 
@@ -119,7 +119,7 @@ int Fun4All_G4_EICDetector(
   // And/Or read generated particles from file
 
   // eic-smear output
-  Input::READEIC = true;
+  //Input::READEIC = true;
   INPUTREADEIC::filename = inputFile;
 
   // HepMC2 files
