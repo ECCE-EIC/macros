0a24c49745b056f64710f3500a46adc940fbe8df
cdean-github <cameron.dean@cern.ch>
2021-10-14 19:00:47 -0400

    Removed verbosity hard codes and moved TRD back 20cm due to overlap in support structure

--- common/G4_TRD.C
+++ common/G4_TRD.C
@@ -4,9 +4,9 @@
 #include <GlobalVariables.C>
 
 #include <fun4all/Fun4AllServer.h>
-#include <g4trd/PHG4TRDSubsystem.h>
 #include <g4main/PHG4Reco.h>
 #include <g4trackfastsim/PHG4TrackFastSim.h>
+#include <g4trd/PHG4TRDSubsystem.h>
 
 R__LOAD_LIBRARY(libg4trd.so)
 R__LOAD_LIBRARY(libg4detectors.so)
@@ -17,39 +17,37 @@ namespace Enable
   //bool TRD_ABSORBER = false;
   bool TRD_GAS = false;
   bool TRD_OVERLAPCHECK = true;
-  int TRD_VERBOSITY = 1;
-}
+  int TRD_VERBOSITY = 0;
+}  // namespace Enable
 
 namespace TRD
 {
-  double R_max =  180.; 
-  double R_min = 15.; 
-  double z_mid = 200.;
-  double half_length = 6.5; // Mother volume, radiator = 10 cm
+  double R_max = 180.;
+  double R_min = 15.;
+  double z_mid = 200. + 20;
+  double half_length = 6.5;  // Mother volume, radiator = 10 cm
   double z_min = z_mid - half_length;
-  double z_max = z_mid + half_length + 10.;  
-}
+  double z_max = z_mid + half_length + 10.;
+}  // namespace TRD
 void TRDInit()
 {
-
-  BlackHoleGeometry::max_radius =  std::max(BlackHoleGeometry::max_radius, TRD::R_max);
-  BlackHoleGeometry::max_z =  std::max(BlackHoleGeometry::max_z, TRD::z_max);
-  BlackHoleGeometry::min_z =  std::min(BlackHoleGeometry::min_z, TRD::z_min);
-
+  BlackHoleGeometry::max_radius = std::max(BlackHoleGeometry::max_radius, TRD::R_max);
+  BlackHoleGeometry::max_z = std::max(BlackHoleGeometry::max_z, TRD::z_max);
+  BlackHoleGeometry::min_z = std::min(BlackHoleGeometry::min_z, TRD::z_min);
 }
 
-void TRDSetup(PHG4Reco *g4Reco)
+void TRDSetup(PHG4Reco* g4Reco)
 {
-
+  int verbosity = std::max(Enable::VERBOSITY, Enable::TRD_VERBOSITY);
   Fun4AllServer* se = Fun4AllServer::instance();
-  se->Verbosity(INT_MAX-10);
-  
+  se->Verbosity(verbosity);
+
   //bool AbsorberActive = Enable::ABSORBER || Enable::TRD_ABSORBER;
   bool GasActive = Enable::ABSORBER || Enable::TRD_GAS;
   bool OverlapCheck = Enable::OVERLAPCHECK || Enable::TRD_OVERLAPCHECK;
   PHG4TRDSubsystem* trd_hcap = new PHG4TRDSubsystem("TRD_hcap", 1);
-  
-  trd_hcap->set_double_param("ThicknessZ", 2.*TRD::half_length);
+
+  trd_hcap->set_double_param("ThicknessZ", 2. * TRD::half_length);
   trd_hcap->set_double_param("RIn", TRD::R_min);
   trd_hcap->set_double_param("ROut", TRD::R_max);
   trd_hcap->set_double_param("PosZ", TRD::z_mid);
@@ -58,53 +56,45 @@ void TRDSetup(PHG4Reco *g4Reco)
   trd_hcap->SetActive(1);
   trd_hcap->SuperDetector("TRD");
   //if(AbsorberActive)
-  if(GasActive)
-    {
-      trd_hcap->SetAbsorberActive(1);
-    }
+  if (GasActive)
+  {
+    trd_hcap->SetAbsorberActive(1);
+  }
   trd_hcap->OverlapCheck(OverlapCheck);
-  //trd_hcap->OverlapCheck(1);
   g4Reco->registerSubsystem(trd_hcap);
-  
-  cout << " Min Z  :" << " " << TRD::z_min  << " Max Z  :" << TRD::z_max << endl; 
-  cout << "=======End setting parameters to geometry : ============" << endl; 
+
+  if (verbosity > 0)
+  {
+    cout << " Min Z  :"
+         << " " << TRD::z_min << " Max Z  :" << TRD::z_max << endl;
+    cout << "=======End setting parameters to geometry : ============" << endl;
+  }
   if (TRACKING::FastKalmanFilter)
-    {
-      TRACKING::FastKalmanFilter-> add_zplane_state("TRD", 284. );
-      TRACKING::ProjectionNames.insert("TRD");
-     
-      //Use hits from  absorber (Active Gas volume)
-      TRACKING::FastKalmanFilter->add_phg4hits(string("G4HIT_") + string(Form("ACTIVEGAS_TRD")),  //      const std::string& phg4hitsNames,
-					       PHG4TrackFastSim::Vertical_Plane,                       //      const DETECTOR_TYPE phg4dettype,
-					       1. / sqrt(12.),                                     //      const float radres,
-					       70e-4,                                              //      const float phires,
-					       100e-4,                                              //      const float lonres,
-					       1,                                                  //      const float eff,
-                                               0);      
-      
-    }
- 
-  cout << " =====End Fast Kalman Filter ======  " << endl; 
+  {
+    TRACKING::FastKalmanFilter->add_zplane_state("TRD", 284.);
+    TRACKING::ProjectionNames.insert("TRD");
+
+    //Use hits from  absorber (Active Gas volume)
+    TRACKING::FastKalmanFilter->add_phg4hits(string("G4HIT_") + string(Form("ACTIVEGAS_TRD")),  //      const std::string& phg4hitsNames,
+                                             PHG4TrackFastSim::Vertical_Plane,                  //      const DETECTOR_TYPE phg4dettype,
+                                             1. / sqrt(12.),                                    //      const float radres,
+                                             70e-4,                                             //      const float phires,
+                                             100e-4,                                            //      const float lonres,
+                                             1,                                                 //      const float eff,
+                                             0);
+  }
+
   return;
- 
 }
 
 void TRD_Reco()
 {
-  
   gSystem->Load("libfun4all.so");
   gSystem->Load("libg4detectors.so");
-  
-  
+
   int verbosity = std::max(Enable::VERBOSITY, Enable::TRD_VERBOSITY);
   Fun4AllServer* se = Fun4AllServer::instance();
-  //se->Verbosity(INT_MAX-10);
-  
-  return;
 
+  return;
 }
 #endif
-
-
-
-
