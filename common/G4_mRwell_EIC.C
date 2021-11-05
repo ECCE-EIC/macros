e10d8ddd21d067e6dba8c5634fdfd5ee05473d00
cdean-github <59485912+cdean-github@users.noreply.github.com>
2021-11-05 11:12:46 -0400

    TTL change and tracking/eval update (#83)
    * Updated G4_EventEvaluator.C to work with new libraries
    
    * Updated G4_hFarFwd to point to new magnet file
    
    * Updated EventEval for new PR
    
    * FIxing overlaps
    
    * Reverting changes made by merge to production branch
    
    * Hash was changed to 'a', fixed
    
    * Added Tristan's clusteriser to macros
    
    * Missing / in setup
    
    * Pushed inner barrel tracker behind last vtx layer
    
    * Updated FGEM and tracking support for move
    
    * Patched TTL and updated tracking/EvtEval

--- common/G4_mRwell_EIC.C
+++ common/G4_mRwell_EIC.C
@@ -392,10 +392,11 @@ double RWellSetup(PHG4Reco* g4Reco,
                                     ilyr);                      //index
     }
 
-//    // sourav: For spatial resolution the mRwell I will use about 55 microns
-//    // (usually it is in between 40-60 microns depending on the angle of incidence of
-//    // primary tracks when mRwell are used in microTPC mode i.e drift gap of 3-4 mm) .
+    // sourav: For spatial resolution the mRwell I will use about 55 microns
+    // (usually it is in between 40-60 microns depending on the angle of incidence of
+    // primary tracks when mRwell are used in microTPC mode i.e drift gap of 3-4 mm) .
     if (TRACKING::FastKalmanFilter)
+    {
       TRACKING::FastKalmanFilter->add_phg4hits(string("G4HIT_") + string(Form("RWELL_%d", ilyr)),  //      const std::string& phg4hitsNames,
                                                PHG4TrackFastSim::Cylinder,                         //      const DETECTOR_TYPE phg4dettype,
                                                1. / sqrt(12.),                                     //      const float radres,
@@ -403,6 +404,9 @@ double RWellSetup(PHG4Reco* g4Reco,
                                                55e-4,                                              //      const float lonres,
                                                1,                                                  //      const float eff,
                                                0);                                                 //      const float noise
+      TRACKING::FastKalmanFilter->add_cylinder_state(Form("RWELL_%d", ilyr), RWELL::nom_radius[ilyr]);
+      TRACKING::ProjectionNames.insert(Form("RWELL_%d", ilyr));
+    }
     if (TRACKING::FastKalmanFilterInnerTrack)
       TRACKING::FastKalmanFilterInnerTrack->add_phg4hits(string("G4HIT_") + string(Form("RWELL_%d", ilyr)),  //      const std::string& phg4hitsNames,
                                                PHG4TrackFastSim::Cylinder,                         //      const DETECTOR_TYPE phg4dettype,
