a92f9085537508a259e48038fc2199c60223e6fa
Joe Osborn <53052717+osbornjd@users.noreply.github.com>
2022-06-29 14:12:18 -0400

    update supports (#111)

--- common/G4_TrackingSupport.C
+++ common/G4_TrackingSupport.C
@@ -254,6 +254,8 @@ double TrackingService(PHG4Reco *g4Reco, double radius)
   double PlasticThickness = 0.48;    // 0.241488 cms
   double CarbonThickness = shellX0;  // 0.3 cms
 
+  double X_X0_percent = 0.1; // % X0
+
   // Cone service from the end Disk to the uRwell1 radius
   cones.push_back(new ServiceProperties("ETrackingCone_Disk4TouRwell", CuThickness, AlThickness, WaterThickness, PlasticThickness, CarbonThickness, 0, -147.47, -90, 68, 41.5));
 
@@ -264,10 +266,10 @@ double TrackingService(PHG4Reco *g4Reco, double radius)
   cones.push_back(new ServiceProperties("ETrackingCone_uRwellToVertex", CuThickness, AlThickness, WaterThickness, PlasticThickness, CarbonThickness, 0, -56.08, -7.699, 41.5, 5.7));
 
   // Sagitta Cylindrical Support Structure
-  //cylinders.push_back(new ServiceProperties("BTrackingCyl_Sagitta", 0, 0, 0, 0, 0.1, 0, -30.3, 30.3, 23.50, 0));
+  cylinders.push_back(new ServiceProperties("BTrackingCyl_Vertex2", 0, 0, 0, 0, X_X0_percent, 0, -15.3, 15.3, 12.6, 0));
 
   // Vertex Cylindrical Support Structure
-  cylinders.push_back(new ServiceProperties("BTrackingCyl_Vertex", 0, 0, 0, 0, shellX0, 0, -7.699, 7.699, 5.7, 0));
+  cylinders.push_back(new ServiceProperties("BTrackingCyl_Vertex", 0, 0, 0, 0, X_X0_percent, 0, -7.699, 7.699, 5.7, 0));
 
   // Cone service in H-region from vertex to inner uRwell
   cones.push_back(new ServiceProperties("HTrackingCone_VertexTouRwell", CuThickness, AlThickness, WaterThickness, PlasticThickness, CarbonThickness, 0, 7.699, 56.08, 5.7, 41.5));
